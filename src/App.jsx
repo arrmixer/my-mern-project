5b9f5bd7c3b53d1a7e92ee883d9d1f98e04d8e48
Angel Rodriguez <arrmixer@gmail.com>
2017-06-23 22:34:06 -0400

    Add existing file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 var contentNode = document.getElementById('contents');
-var contentNode2 = document.getElementById('contents2');
-
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
 
 
-var component2 = <h2>Hello World!</h2>;
-const component = <p>{message}</p>;           //A simple JSX components
-ReactDOM.render(component, contentNode);      // Render the component inside the content Node
-ReactDOM.render(component2, contentNode2);
 
 class IssueList extends React.Component {
     render(){
        return(
-               <div>This is just a placeholder for the issue list.</div>
+               <div>This is just a placeholder for the issue list.</div>   //Render the component inside the content node
            );
     }
 }
