99d567e4e384cca355409d728b9f483d1438fe1b
Angel Rodriguez <arrmixer@gmail.com>
2017-06-25 14:38:22 -0400

    added an temp array and use map function

--- static/App.js
+++ static/App.js
@@ -45,19 +45,44 @@ var IssueRow = function (_React$Component2) {
     _createClass(IssueRow, [{
         key: "render",
         value: function render() {
-            var borderedStyle = { border: "1px solid silver", padding: 4 };
+            var issue = this.props.issue;
             return React.createElement(
                 "tr",
                 null,
                 React.createElement(
                     "td",
-                    { style: borderedStyle },
-                    this.props.issue_id
+                    null,
+                    issue.id
                 ),
                 React.createElement(
                     "td",
-                    { style: borderedStyle },
-                    this.props.children
+                    null,
+                    issue.status
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    issue.owner
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    issue.created.toDateString()
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    issue.effort
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    issue.completionDate ? issue.completionDate.toDateString() : " "
+                ),
+                React.createElement(
+                    "td",
+                    null,
+                    issue.title
                 )
             );
         }
@@ -78,10 +103,12 @@ var IssueTable = function (_React$Component3) {
     _createClass(IssueTable, [{
         key: "render",
         value: function render() {
-            var borderedStyle = { border: "1px solid silver", padding: 6 };
+            var issueRows = this.props.issues.map(function (issue) {
+                return React.createElement(IssueRow, { key: issue.id, issue: issue });
+            });
             return React.createElement(
                 "table",
-                { style: { borderCollapse: "collapse" } },
+                { className: "bordered-table" },
                 React.createElement(
                     "thead",
                     null,
@@ -90,12 +117,37 @@ var IssueTable = function (_React$Component3) {
                         null,
                         React.createElement(
                             "th",
-                            { style: borderedStyle },
+                            null,
                             "Id"
                         ),
                         React.createElement(
                             "th",
-                            { style: borderedStyle },
+                            null,
+                            "Status"
+                        ),
+                        React.createElement(
+                            "th",
+                            null,
+                            "Owner"
+                        ),
+                        React.createElement(
+                            "th",
+                            null,
+                            "Created"
+                        ),
+                        React.createElement(
+                            "th",
+                            null,
+                            "Effort"
+                        ),
+                        React.createElement(
+                            "th",
+                            null,
+                            "Completion"
+                        ),
+                        React.createElement(
+                            "th",
+                            null,
                             "Title"
                         )
                     )
@@ -103,22 +155,7 @@ var IssueTable = function (_React$Component3) {
                 React.createElement(
                     "tbody",
                     null,
-                    React.createElement(
-                        IssueRow,
-                        { issue_id: 1 },
-                        "Error in console when clicking Add"
-                    ),
-                    React.createElement(
-                        IssueRow,
-                        { issue_id: 2 },
-                        "Missing bottom ",
-                        React.createElement(
-                            "b",
-                            null,
-                            "border"
-                        ),
-                        " on panel"
-                    )
+                    issueRows
                 )
             );
         }
@@ -150,6 +187,16 @@ var IssueAdd = function (_React$Component4) {
     return IssueAdd;
 }(React.Component);
 
+var issues = [{
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add'
+}, {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel'
+}];
+
 var IssueList = function (_React$Component5) {
     _inherits(IssueList, _React$Component5);
 
@@ -172,7 +219,7 @@ var IssueList = function (_React$Component5) {
                 ),
                 React.createElement(IssueFilter, null),
                 React.createElement("hr", null),
-                React.createElement(IssueTable, null),
+                React.createElement(IssueTable, { issues: issues }),
                 React.createElement("hr", null),
                 React.createElement(IssueAdd, null)
             );
